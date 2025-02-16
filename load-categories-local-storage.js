ecb55f966625aa3c9662a3d070a61a5614ffab66
devovi2024 <arfanhosenovi@gmail.com>
2025-02-16 23:06:41 +0600

    create array name categorires

--- load-categories-local-storage.js
+++ load-categories-local-storage.js
@@ -1,2 +1,4 @@
 const {localStorage, LocalStorage} = require('node-localstorage');
-const localStorage = new LocalStorage('./storage')
+const localStorage = new LocalStorage('./storage');
+
+const defaultCategories = ["Technology", 'Server','Client','Database'];
