5ed87b2d66bd1d0c1cc0cdbd9b5ae36be701f865
devovi2024 <arfanhosenovi@gmail.com>
2025-02-16 23:36:14 +0600

    new data adding

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ const { LocalStorage } = require('node-localstorage');
 const localStorage = new LocalStorage('./storage');
 
 const defaultCategories = ["Technology", "Server", "Client", "Database"];
-
 localStorage.setItem('categories', JSON.stringify(defaultCategories));
 
 const data = JSON.parse(localStorage.getItem('categories'));
+data.push("Networking");
+
+localStorage.setItem('categories', JSON.stringify(data));
+
+console.log(data);
